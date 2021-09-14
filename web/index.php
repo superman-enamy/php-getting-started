b9762e400e799429c7f2c162faa2d1755ca28982
superman-enamy <47158027+superman-enamy@users.noreply.github.com>
2021-09-14 12:37:40 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,214 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+// usage index.php?c=Channel_ID
+// Don't Change Any Thing !
 
-<head>
-	<script data-ad-client="ca-pub-8003681892226291" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<meta charset="utf-8">
-	<meta name="author" content="Stream India" />
-	<meta name="keywords" content="stream india , stream , stream movies, watch online movies , sony liv, amazon prime , dramas , online series, jl 50, jl50, ipl2020 live, ipl 2020 hindi live free" />
-	<meta name="revised" content="19Sep2020" />
-	<meta name="designer" content="co-designer/Sukhjinder Soni" />
-	<meta name="description" content="Watch Live IPL 2020 only on ipl.enamyworld.xyz - One and Only platform superfastfast and free to use. No signup required">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<link rel="stylesheet" href="https://cdn.plyr.io/3.6.2/plyr.css" />
-	<script src="https://kit.fontawesome.com/bf2610f0a2.js" crossorigin="anonymous"></script>
-	<!-- Halfmoon CSS -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1">
-	<link rel="icon" href="/static/img/favicon.png">
-	<title>IPL 2020 Live | Hindi | English | Star Sports</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	<link href="https://www.tvhub.in/static/css/custom.css" rel="stylesheet">
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-74975322-4"></script>
-	<script>
-	window.dataLayer = window.dataLayer || [];
+$url = $_GET["c"];
+if($url !=""){
 
-	function gtag() {
-		dataLayer.push(arguments);
-	}
-	gtag('js', new Date());
-	gtag('config', 'UA-74975322-4');
-	document.addEventListener("contextmenu", function(e) {
-		e.preventDefault();
-	}, false);
-	document.addEventListener("keydown", function(e) {
-		//document.onkeydown = function(e) {
-		// "I" key
-		if(e.ctrlKey && e.shiftKey && e.keyCode == 73) {
-			disabledEvent(e);
-		}
-		// "J" key
-		if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-			disabledEvent(e);
-		}
-		// "S" key + macOS
-		if(e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-			disabledEvent(e);
-		}
-		// "U" key
-		if(e.ctrlKey && e.keyCode == 85) {
-			disabledEvent(e);
-		}
-		// "F12" key
-		if(event.keyCode == 123) {
-			disabledEvent(e);
-		}
-	}, false);
+$data = file_get_contents("https://catalogapi.zee5.com/v1/channel/${url}");
+$z5 =json_decode($data, true);
+$stream = $z5['stream_url_hls'];
 
-	function disabledEvent(e) {
-		if(e.stopPropagation) {
-			e.stopPropagation();
-		} else if(window.event) {
-			window.event.cancelBubble = true;
-		}
-		e.preventDefault();
-		return false;
-	}
-	</script>
-	<!--[if lt IE 9]>
-									<!--      ___subtmpl0<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  ___subtmpl1<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>-->
-	<meta name="verify-admitad" content="9c91b80fe1" />
-	<meta name="wot-verification" content="5ce94c5e0d0118de789d" /> </head>
+$tdata = file_get_contents("https://useraction.zee5.com/token/live.php");
+$tok =json_decode($tdata, true);
+$vid_token = $tok['video_token'];
+$m3u8 = $stream.$vid_token;
 
-<body>
-	<nav class="navbar navbar-expand-lg navbar-light sticky-top bg-white">
-		<div class="container">
-			<a class="navbar-brand brand-text" href="/"> <img width="100px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQP0SmkgFaThDnezwM2QYgs9foAO0U3-qeVQg&usqp=CAU" alt="IPL LIVE"> </a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item"> <a class="nav-link" href="/">Home</a> </li>
-					<li class="nav-item "> <a class="nav-link" href="/hindi/">Hindi</a> </li>
-					<li class="nav-item "> <a class="nav-link" href="/english/">English</a> </li>
-					<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Others
-                        </a> </li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-	<div class="container">
-		<div class="row text-center">
-			<div class="col col-padding"></div>
-		</div>
-	</div>
-	<div class="container">
-		<div class="text-center col-padding">
-			<h1>Watch IPL Live TV channels for
-                free</h1> </div>
-		<div class="input-group input-group-lg col-padding">
-			<input id="myInput" type="text" onkeyup="searchChannel()" class="form-control" placeholder="Search.." aria-label="Search" aria-describedby="basic-addon2">
-			<div class="input-group-append">
-				<button class="btn btn-outline-primary" onclick="searchChannel()" type="button">Search</button>
-			</div>
-		</div>
-		<center>
-			<div class="row">
-				<div class="col" style="margin-top: 20px">
-					<ul class="list-group">
-						<li class="list-group-item"> <a href="http://enamyworld.xyz/ipl-live/start-sports-hd.html">Star Sport HD 1 
-                            (English)</a> </li>
-						<li class="list-group-item"> <a href="http://enamyworld.xyz/ipl-live/ipl-3g.html">IPL LIVE 3G
-                            (हिन्दी)</a> </li>
-						<li class="list-group-item"> <a href="http://enamyworld.xyz/ipl-live/ipl-hd.html">IPL LIVE 1
-                           (English)</a> </li>
-						<li class="list-group-item"> <a href="http://enamyworld.xyz/ipl-live/start-sports.html">Star Sports LQ
-                            (English)</a> </li>
-                            <li class="list-group-item"> <a href="http://t.me/b0rntole4rn">Join Telegram</a> </li>
-                             <li class="list-group-item"> <a href="https://chat.whatsapp.com/CEl32EYBvX21bRkcVbKWCz">Join Whatsapp</a> </li>
-					</ul>
-				</div>
-			</div>
-	</div>
-	</center>
-	
-	<footer class="bg-light" style="margin-top: 180px; padding-top: 20px;">
-		<div class="container">
-			<div class="row">
-				<div class="text-center">
-					<a class="navbar-brand brand-text" href="/"> <img width="100px" style="-webkit-filter: grayscale(100%);filter: grayscale(100%); vertical-align: text-bottom;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQP0SmkgFaThDnezwM2QYgs9foAO0U3-qeVQg&usqp=CAU" alt="IPL LIVE"> </a> <a class="min-font" href="/ipl">Home</a> <a class="min-font" href="/ipl">All Channels</a> <a class="min-font" href="dmca.html">DMCA / Copyright Policy</a> </div>
-			</div>
-			<hr>
-			<div class="footer-lower">
-				<div class="row">
-					<div class="col-sm-12"></div>
-				</div>
-				<div class="row mbr-white">
-					<div class="col-sm-12 copyright min-font">
-						<p> © Copyright 2020 enamyworld.xyz - All Rights Reserved. Channel logos are trademarks of respective owners. </p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
-	<script>
-	(function(w, d, s, id, src) {
-		w.Repixel = r = {
-			init: function(id) {
-				w.repixelId = id;
-			}
-		};
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if(d.getElementById(id)) {
-			return;
-		}
-		js = d.createElement(s);
-		js.id = id;
-		js.async = true;
-		js.onload = function() {
-			Repixel.init(w.repixelId);
-		};
-		js.src = src;
-		fjs.parentNode.insertBefore(js, fjs);
-	}(window, document, 'script', 'repixel', 'https://sdk.repixel.co/r.js'));
-	Repixel.init('5ddd595af2b3450008cbb98b');
-	</script>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-	<script async type="text/javascript" src="//platform-api.sharethis.com/js/sharethis.js#property=5b5c0882d6d379001104aec7&product=sticky-share-buttons"></script>
-	<script type="text/javascript">
-	var cachebuster = Math.round(new Date().getTime() / 1000);
-	var mMTagScript = document.createElement('script');
-	mMTagScript.src = 'https://mahimeta.com/networks/tag.js?cache=' + cachebuster;
-	document.getElementsByTagName("head")[0].appendChild(mMTagScript);
-	</script>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-37336092-1"></script>
-	<script>
-	window.dataLayer = window.dataLayer || [];
+//echo $m3u8;
+header("Location: $m3u8"); //--> For Direct Play
 
-	function gtag() {
-		dataLayer.push(arguments);
-	}
-	gtag('js', new Date());
-	gtag('config', 'UA-37336092-1');
-	</script>
-	<script>
-	var page_id = location.pathname;
-	var VUUKLE_CONFIG = {
-		apiKey: "4aae8326-fb1b-4f4b-ac3b-61ffa2a101ef",
-		articleId: page_id,
-	};
-	// DON'T EDIT BELOW THIS LINE
-	(function() {
-		var d = document,
-			s = d.createElement('script');
-		s.src = 'https://cdn.vuukle.com/platform.js';
-		(d.head || d.body).appendChild(s);
-	})();
-	</script>
-	<script src="https://www.tvhub.in/static/js/home.js" crossorigin="anonymous"></script>
-</body>
-<!-- BEGIN: Powered by Supercounters.com -->
-<center>
-	<script type="text/javascript" src="//widget.supercounters.com/ssl/online_t.js"></script>
-	<script type="text/javascript">
-	sc_online_t(1594014, "User Going To Watch", "170ddb");
-	</script>
-	<br>
-	<noscript> <a href="https://www.supercounters.com/">supercounters.com</a> </noscript>
-</center>
-<!-- END: Powered by Supercounters.com -->
+}
+else{
+  $ex= array("error" => "Something went wrong, Check URL", "created_by" => "Techie Sneh" );
+  $error =json_encode($ex);
 
-</html>
+  echo $error;
+}
+
+?>
